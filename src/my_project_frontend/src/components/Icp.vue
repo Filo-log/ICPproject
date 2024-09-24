9e7b43af61496244eadbd33168e7471fe376102e
Filip Podróżny <filip.podrozny@wp.pl>
2024-09-24 17:49:42 +0000

    Feat: Frontend

---
+++ src/my_project_frontend/src/components/Icp.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue';
+import { my_project_backend } from 'declarations/my_project_backend/index';
+let greeting = ref('');
+
+async function handleSubmit(e) {
+  e.preventDefault();
+  const target = e.target;
+  const name = target.querySelector('#name').value;
+  await my_project_backend.greet(name).then((response) => {
+    greeting.value = response;
+  });
+}
+</script>
+
+<template>
+    <img src="/logo2.svg" alt="DFINITY logo" class="icp-img" />
+    <form action="#" @submit="handleSubmit">
+      <label for="name">Enter your name: &nbsp;</label>
+      <input id="name" alt="Name" type="type" />
+      <button type="submit">Click Me!</button>
+      <header>
+        <h1 id="greeting">{{ greeting }}</h1>
+        <h2>Student</h2>
+      </header>
+    </form>
+</template>
